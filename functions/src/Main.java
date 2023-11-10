d7e544c84edd34097729f9f55622b7f4d4c56fdc
DiegoCard117 <diegocardoso117@gmail.com>
2023-11-10 14:08:29 -0300

    functions

---
+++ functions/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int result = soma(1,4);
+        System.out.println(result);
+    }
+
+    public static int soma(int number1, int number2) {
+        return number1 + number2;
+    }
+}
